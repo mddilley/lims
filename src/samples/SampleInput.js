c30be558ec30a32da799b485cf095b0905cd95c5
Michael Dilley <mike.dilley@gmail.com>
2019-04-23 18:34:33 -0500

    Add form to SamplesInput component, correct class and export name to SamplesInput

--- src/samples/SampleInput.js
+++ src/samples/SampleInput.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
-class SamplesContainer extends Component {
+class SamplesInput extends Component {
   render(){
     return(
       <div className="outline">
-        <p>Samples form</p>
+        <form>
+          <label>Sample Patient First Name:</label>
+          <input type="text" name="firstName"/><br/>
+          <label>Sample Patient Last Name:</label>
+          <input type="text" name="lastName"/><br/>
+          <label>Sample Patient Age:</label>
+          <input type="text" name="lastName"/><br/>
+          <input type="submit" />
+        </form>
       </div>
     )
   }
 }
 
-export default SamplesContainer;
+export default SamplesInput;
