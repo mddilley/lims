bcc00b0ad339f3d732b7ce034cc5333189959527
Michael Dilley <mike.dilley@gmail.com>
2019-04-23 17:35:12 -0500

    Add icons to header and FontAwesome cdn link in /public/index.html

--- src/static/Header.js
+++ src/static/Header.js
@@ -5,10 +5,10 @@ class Header extends Component {
     render() {
         return (
           <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">Oculus</Navbar.Brand>
+            <Navbar.Brand href="/"><i className="fas fa-flask"></i><i className="far fa-eye"></i> Oculus</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">          
+              <Nav className="mr-auto">
                 <Nav.Link href="/batches">Batches</Nav.Link>
                 <Nav.Link href="/about">About</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
