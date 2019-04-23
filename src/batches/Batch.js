f79f439f033d40272a5bbd74cb550ab752e3f5ab
Michael Dilley <mike.dilley@gmail.com>
2019-04-23 15:10:22 -0500

    Add cuid package to generate batchId and assign to each batch in manageBatch reducer, add batchId display in Batch component

--- src/batches/Batch.js
+++ src/batches/Batch.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react'
 class Batch extends Component {
 
   render(){
+    const batch = this.props.batch
     return(
       <div className="outline">
-        <p>Batch Date: {this.props.batch.date}</p>
-        <p>Batch Creator: {this.props.batch.creator}</p>
+        <p>Batch ID: {batch.batchId}</p>
+        <p>Batch Date: {batch.date}</p>
+        <p>Batch Creator: {batch.creator}</p>
       </div>
     )
   }
--- src/reducers/manageBatch.js
+++ src/reducers/manageBatch.js
@@ -1,8 +1,12 @@
+import cuid from 'cuid';
+export const cuidFn = cuid;
+
 export default function manageBatch(state = {batches: []}, action){
   switch(action.type){
     case 'ADD_BATCH':
       console.log("inside ADD_BATCH case in reducer")
-      return {...state, batches: [...state.batches, action.batch]}
+      const batch = {...action.batch, batchId: cuid()}
+      return {...state, batches: [...state.batches, batch]}
     default:
       return state
   }
