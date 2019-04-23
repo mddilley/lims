0959611dd237627c0fec49a8785347dd9b4dbe96
Michael Dilley <mike.dilley@gmail.com>
2019-04-23 13:56:16 -0500

    Add event listeners to form inputs in BatchInput component, add handleChange function to update state, add constructor and state (creator, date) to BatchInput component

--- src/batches/BatchInput.js
+++ src/batches/BatchInput.js
@@ -9,14 +9,20 @@ class BatchInput extends Component {
     }
   }
 
+  handleChange = (event) => {
+    const state = {}
+    state[event.target.name] = event.target.value
+    this.setState(state)
+  }
+
   render(){
     return(
       <div className="outline">
         <form>
           <label>Batch Creator:</label>
-          <input type="text" /><br/>
+          <input type="text" name="creator" onChange={this.handleChange}/><br/>
           <label>Batch Date:</label>
-          <input type="date" /><br/>
+          <input type="date" name="date" onChange={this.handleChange}/><br/>
           <input type="submit" />
         </form>
       </div>
