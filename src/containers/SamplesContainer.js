b92bfdcc9947e488d858c0be781cf6902813541a
Michael Dilley <mike.dilley@gmail.com>
2019-04-26 12:50:38 -0500

    Add loader animation to SamplesContainer component

--- src/containers/SamplesContainer.js
+++ src/containers/SamplesContainer.js
@@ -6,11 +6,16 @@ import { connect } from 'react-redux'
 class SamplesContainer extends Component {
   render(){
     const batchId = this.props.location.state.batchId
+    let loader = ""
+    if(this.props.loading === true){
+      loader = <div className="loader container"></div>
+    }
     return(
       <div className="outline">
         <h1>Batch ID# {batchId}</h1>
         <SampleInput batchId={batchId} addSample={this.props.addSample}/>
         <Samples samples={this.props.samples} deleteSample={this.props.deleteSample}/>
+        {loader}
       </div>
     )
   }
