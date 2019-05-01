59fccc4bce15113725861dfea043af2752e92154
Michael Dilley <mike.dilley@gmail.com>
2019-05-01 17:14:50 -0500

    Add .white-background to headers in About component and add breaks to improve mobile readability

--- src/static/About.js
+++ src/static/About.js
@@ -3,13 +3,16 @@ import { Container } from 'react-bootstrap'
 
 const About = () =>
   <div>
-    <h1 className="display-2">About Oculus</h1><br/><br/>
+    <br/><br/>
+    <h1 className="display-2 white-background">About Oculus</h1><br/><br/>
     <Container>
+    <br/>
     <h5 className="grey-background">Oculus is a laboratory information system intended for small laboratory applications. It is built using a React/Redux frontend and a Ruby on Rails API.</h5>
     </Container>
     <br/>
     <Container>
-    <h1 className="display-4">Testimonials</h1><br/><br/>
+    <h1 className="display-4 white-background">Testimonials</h1><br/><br/>
+    <br/>
     <div className="grey-background batch-space">
       <span>"After running into problems with Jarvis, I realized that I needed to downsize for my laboratory's information needs. Oculus helped me get back on track." -Tony Stark</span>
     </div>
