977c7c1de56d09cb984db407a157f7c86cdf88bf
Michael Dilley <mike.dilley@gmail.com>
2019-05-01 17:17:46 -0500

    Change Header component from ES6 class to function (stateless)

--- src/static/Header.js
+++ src/static/Header.js
@@ -1,21 +1,16 @@
 import { Navbar, Nav } from 'react-bootstrap'
 import React, { Component } from 'react'
 
-class Header extends Component {
-    render() {
-        return (
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/"><i className="fas fa-flask"></i><i className="far fa-eye"></i> Oculus</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/batches">Batches</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-      );
-    }
-}
+const Header = () =>
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="/"><i className="fas fa-flask"></i><i className="far fa-eye"></i> Oculus</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/batches">Batches</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 
  export default Header;
